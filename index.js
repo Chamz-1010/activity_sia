eb76eaf1471092e27f1247574e33679ac1709d0f
Chamz-1010 <shroudedstep19@gmail.com>
2026-08-10 15:43:47 +0800

    ci: clean up somme trash

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-asdasf
+//package imports
+const express = require('express');
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+app.listen(1234, () => {
+  console.log('Server is running on http://localhost:1234')
+})
